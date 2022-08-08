c74a1af7270044a1c8f175e939844667c5264e3f
heb <qq7476160>
2022-08-08 16:00:49 +0800

    ++++++

--- ijkplayer-view/src/main/java/com/kjsc/ijkplayer/VideoViewForTv.java
+++ ijkplayer-view/src/main/java/com/kjsc/ijkplayer/VideoViewForTv.java
@@ -6,7 +6,6 @@ import android.os.Message;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
@@ -24,12 +23,13 @@ import tv.danmaku.ijk.media.player.IMediaPlayer;
 public class VideoViewForTv extends IjkVideoView {
     TextView tvNowTime, tvAllTime;
     SeekBar seekBar;
-    boolean isUpdateSeekBar = true;
+    boolean isSeek = false;
     ImageView ivPlayStatus;
     LinearLayout control;
 
+    int controlTimeout = 5000;
+
     public static final int UPDATE_WHAT = 0;
-    public static final int HIDE_PLAY_STATUS_WHAT = 1;
     public static final int HIDE_CONTROL_WHAT = 2;
 
     public VideoViewForTv(@NonNull Context context) {
@@ -54,8 +54,7 @@ public class VideoViewForTv extends IjkVideoView {
         seekBar = findViewById(R.id.seek_bar);
         ivPlayStatus = findViewById(R.id.play_status);
         control = findViewById(R.id.control);
-        isShowControl(false);
-        setFocusable(true);
+        setUserControl(true);
     }
 
     SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
@@ -80,17 +79,14 @@ public class VideoViewForTv extends IjkVideoView {
                         int currentPosition = getCurrentPosition();
                         String curTime = millis2String(currentPosition);
                         tvNowTime.setText(curTime);
-                        if (isUpdateSeekBar) {
+                        if (!isSeek) {
                             seekBar.setProgress(currentPosition);
                         }
                     }
                     handler.sendEmptyMessageDelayed(UPDATE_WHAT, 500);
                     break;
-                case HIDE_PLAY_STATUS_WHAT:
-                    ivPlayStatus.setVisibility(GONE);
-                    break;
                 case HIDE_CONTROL_WHAT:
-                    isShowControl(false);
+                    hideControl();
                     break;
             }
             return false;
@@ -102,42 +98,41 @@ public class VideoViewForTv extends IjkVideoView {
         super.onPrepared(mp);
         start();
         handler.sendEmptyMessage(UPDATE_WHAT);
-        isShowControl(true);
-        seekBar.requestFocus();
+        showControl();
     }
 
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
         System.out.println("dispatchKeyEvent isFocused=" + seekBar.isFocused());
-        handler.removeMessages(HIDE_CONTROL_WHAT);
-        isShowControl(true);
-        int action = event.getAction();
-        int keycode = event.getKeyCode();
-        if (action == KeyEvent.ACTION_DOWN && (keycode == KeyEvent.KEYCODE_DPAD_LEFT || keycode == KeyEvent.KEYCODE_DPAD_RIGHT)) {
-            if (isNoControl) {
-                return true;
-            }
-            isUpdateSeekBar = false;
-        } else if (event.getAction() == KeyEvent.ACTION_UP && (keycode == KeyEvent.KEYCODE_DPAD_LEFT || keycode == KeyEvent.KEYCODE_DPAD_RIGHT)) {
-            if (isNoControl) {
-                return true;
-            }
-            isUpdateSeekBar = true;
-            seekTo(seekBar.getProgress());
-            start();
-        }
-
-        if (action == KeyEvent.ACTION_DOWN) {
-            switch (keycode) {
-                case KeyEvent.KEYCODE_DPAD_CENTER:
-                case KeyEvent.KEYCODE_ENTER:
-                    if (isPlaying()) {
-                        pause();
-                    } else {
+        if (controlFocusable) {
+            showControl();
+            int action = event.getAction();
+            int keycode = event.getKeyCode();
+            if (action == KeyEvent.ACTION_DOWN) {
+                switch (keycode) {
+                    case KeyEvent.KEYCODE_DPAD_LEFT:
+                    case KeyEvent.KEYCODE_DPAD_RIGHT:
+                        isSeek = true;
+                        break;
+                    case KeyEvent.KEYCODE_DPAD_CENTER:
+                    case KeyEvent.KEYCODE_ENTER:
+                        if (isPlaying()) {
+                            pause();
+                        } else {
+                            start();
+                        }
+                        return true;
+                }
+            } else if (action == KeyEvent.ACTION_UP) {
+                switch (keycode) {
+                    case KeyEvent.KEYCODE_DPAD_LEFT:
+                    case KeyEvent.KEYCODE_DPAD_RIGHT:
+                        isSeek = false;
+                        seekTo(seekBar.getProgress());
                         start();
-                    }
-                    return true;
+                        break;
+                }
             }
         }
         return super.dispatchKeyEvent(event);
@@ -147,30 +142,53 @@ public class VideoViewForTv extends IjkVideoView {
     public void pause() {
         super.pause();
         ivPlayStatus.setVisibility(VISIBLE);
-        ivPlayStatus.setImageResource(R.mipmap.icon_player);
     }
 
     @Override
     public void start() {
         super.start();
-        if (ivPlayStatus.isShown()) {
-            ivPlayStatus.setImageResource(R.mipmap.icon_pause);
-        }
-        handler.sendEmptyMessageDelayed(HIDE_PLAY_STATUS_WHAT, 500);
+        ivPlayStatus.setVisibility(GONE);
     }
 
-    public void isShowControl(boolean isShow) {
-        control.setVisibility(isShow ? VISIBLE : INVISIBLE);
-        if (isShow) {
+    public void setControlTimeout(int timeout) {
+        controlTimeout = timeout;
+    }
+
+    public void showControl() {
+        if (useControl) {
+            control.setVisibility(VISIBLE);
             seekBar.requestFocus();
-            handler.sendEmptyMessageDelayed(HIDE_CONTROL_WHAT, 3000);
+            if (controlTimeout > 0) {
+                handler.removeMessages(HIDE_CONTROL_WHAT);
+                handler.sendEmptyMessageDelayed(HIDE_CONTROL_WHAT, controlTimeout);
+            }
         }
     }
 
-    boolean isNoControl = false;
 
-    public void setNoControl(boolean noControl) {
-        isNoControl = noControl;
+    public void hideControl() {
+        control.setVisibility(INVISIBLE);
     }
 
+    boolean controlFocusable = true;
+
+    public void setControlFocus(boolean needControl) {
+        controlFocusable = needControl;
+        if (controlFocusable) {
+            setFocusable(true);
+            seekBar.setFocusable(true);
+        } else {
+            setFocusable(false);
+            seekBar.setFocusable(false);
+        }
+    }
+
+    boolean useControl = true;
+
+    public void setUserControl(boolean useControl) {
+        this.useControl = useControl;
+        if (!useControl) {
+            setControlFocus(false);
+        }
+    }
 }
