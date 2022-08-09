586a394f70ebff424a5c7a3d8b287e71a6d5fc71
heb <qq7476160>
2022-08-09 14:27:24 +0800

    ++++++

---
+++ app/src/main/java/com/kjsc/ijkplayertest/MyApp.java
@@ -0,0 +1,14 @@
+package com.kjsc.ijkplayertest;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
+public class MyApp extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+}
